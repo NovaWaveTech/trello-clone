ea2f6a30ed6616dfb6297166c6dc4d8e1bce7210
Caubi Neto <caubineto@outlook.com>
2024-03-21 09:04:33 -0300

    :test_tube: test: Criando novo teste

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,23 @@
+import { User2 } from "lucide-react";
+
+export const BoardList = () => {
+    return (
+        <div className="space-y-4">
+            <div className="flex items-center font-semibold text-lg text-neutral-700">
+                <User2 className="h-6 w-6 mr-2"/>
+                Seus quadros
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div
+                    role="button"
+                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                >
+                    <p className="text-sm">Crie um novo quadro</p>
+                    <span className="text-xs">
+                        5 restantes
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
