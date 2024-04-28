0500635cc2216b3e0b016a3ff11ae4fec409a0fc
Caubi Neto <caubineto@outlook.com>
2024-04-28 19:37:16 -0300

    :lipstick: Simbolo de check quando seleciona uma imagem para o quadro

--- components/forms/form-picker.tsx
+++ components/forms/form-picker.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { Loader2 } from "lucide-react";
+import { Check, Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
 import { useEffect, useState } from "react";
 
@@ -27,9 +27,9 @@ export const FormPicker = ({
 
     useEffect(() => {
         const fetchImages = async () => {
-            try{
+            try {
                 const result = await unsplash.photos.getRandom({
-                    collectionIds: ["317099"], 
+                    collectionIds: ["317099"],
                     count: 9,
                 });
 
@@ -62,7 +62,7 @@ export const FormPicker = ({
         <div className="relative">
             <div className="grid grid-cols-3 gap-2 mb-2">
                 {images.map((image) => (
-                    <div 
+                    <div
                         key={image.id}
                         className={cn(
                             "cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
@@ -72,13 +72,18 @@ export const FormPicker = ({
                             if (pending) return;
                             setSelectedImageId(image.id);
                         }}
-                        >
-                        <Image 
+                    >
+                        <Image
                             src={image.urls.thumb}
                             alt="Imagem do Unsplash"
                             className="object-cover rounded-sm"
-                            fill 
+                            fill
                         />
+                        {selectedImageId === image.id && (
+                            <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                                <Check className="h-4 w-4 text-white" />
+                            </div>
+                        )}
                         <Link href={image.links.html}
                             target="_blank"
                             className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
